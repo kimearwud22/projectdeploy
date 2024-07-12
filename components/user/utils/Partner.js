c58063d921b27bcd7603e71c90e59307cfc7e7f4
Setiyo Pramono <setiyopramono04@gmail.com>
2024-07-12 13:20:20 +0700

    update partnership

---
+++ components/user/utils/Partner.js
@@ -0,0 +1,53 @@
+import React from 'react'
+
+export default function Partner() {
+  return ( 
+    <div className='bg-white'>  
+  <div className=" overflow-hidden">
+    <h1 className='text-center pt-5 pb-3'>Partner Kami</h1>
+    <div className="row ">
+      <div className="col-3 mt-3 text-center">
+        <div className="text-secondary ">
+        <img src="/assets/img/partner-1.jpg" alt="Coffee Icon" style={{height:"200px", width:"200px"}}/>
+        </div>
+      </div>
+      <div className="col-3 mt-3 text-center">
+        <div className="text-secondary ">
+        <img src="/assets/img/partner-2.jpg" alt="Coffee Icon" style={{height:"200px", width:"200px"}}/>
+        </div>
+      </div>
+      <div className="col-3 mt-3 text-center">
+        <div className="text-secondary ">
+        <img src="/assets/img/partner-3.jpg" alt="Coffee Icon" style={{height:"200px", width:"200px"}}/>
+        </div>
+      </div>
+      <div className="col-3 mt-3 text-center">
+        <div className="text-secondary ">
+        <img src="/assets/img/partner-4.jpg" alt="Coffee Icon" style={{height:"200px", width:"200px"}}/>
+        </div>
+      </div>
+      <div className="col-3 mt-3 text-center">
+        <div className="text-secondary ">
+        <img src="/assets/img/partner-5.jpg" alt="Coffee Icon" style={{height:"200px", width:"200px"}}/>
+        </div>
+      </div>
+      <div className="col-3 mt-3 text-center">
+        <div className="text-secondary ">
+        <img src="/assets/img/partner-6.jpg" alt="Coffee Icon" style={{height:"200px", width:"200px"}}/>
+        </div>
+      </div>
+      <div className="col-3 mt-3 text-center">
+        <div className="text-secondary ">
+        <img src="/assets/img/partner-7.jpg" alt="Coffee Icon" style={{height:"200px", width:"200px"}}/>
+        </div>
+      </div>
+      <div className="col-3 mt-3 text-center">
+        <div className="text-secondary ">
+        <img src="/assets/img/partner-8.png" alt="Coffee Icon" style={{height:"200px", width:"200px"}}/>
+        </div>
+      </div>
+    </div>
+  </div>
+    </div>
+  )
+}
